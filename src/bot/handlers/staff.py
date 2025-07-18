1c9a1360474f918e8e7d979d4acc5afa23465a22
AnnyRomanova <anna.romanova0818@gmail.com>
2025-07-18 16:58:49 +0300

    add logging

---
+++ src/bot/handlers/staff.py
@@ -0,0 +1,14 @@
+from aiogram import types
+
+
+async def my_tasks(message: types.Message):
+    pass
+
+async def today(message: types.Message):
+    pass
+
+async def overdue(message: types.Message):
+    pass
+
+async def update_task(message: types.Message):
+    pass
